767fc39c19c46b998aedcd46b168175e648de435
Madan M S <madan.ms.work@gmail.com>
2026-01-04 08:20:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6677479558-a0742",
+  "appId": "1:97985640776:web:0ae6e4e024ec79d6a95b7d",
+  "apiKey": "AIzaSyDZY5b_iMS8uuuyIgVVawg6LWLr6OioiBE",
+  "authDomain": "studio-6677479558-a0742.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "97985640776"
+};
